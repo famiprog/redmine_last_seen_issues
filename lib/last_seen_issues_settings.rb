6e256b6730be78797cff8e039254da2b50f59822
Teodor Duta <teodor.duta@resonate-mp4.com>
2024-04-22 13:23:15 +0300

    #31741 - resolve remarks

--- lib/last_seen_issues_settings.rb
+++ lib/last_seen_issues_settings.rb
@@ -8,9 +8,9 @@ class LastSeenIssuesSettings
 
     def self.get_max_number_of_issues
         issues_display_limit = LastSeenIssuesSettings.get_setting(:number_of_issues_displayed)
-        # for blank setting or not a number, default 5
+        # for blank setting or not a number, default 30
         if (issues_display_limit.blank? || !(/\A\d+\z/).match(issues_display_limit))
-          issues_display_limit = 5
+          issues_display_limit = 30
         end
         return issues_display_limit
     end
