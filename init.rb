3a76fbec8a6a64de1e2e206451a2e355eac88694
mircea-negreanu <mircea.negreanu@crispico.com>
2024-10-21 11:16:23 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_last_seen_issues do
     name 'Redmine last seen issues plugin'
     author 'famiprog'
     description 'For detailed documentation see the link below.'
-    version '1.0.1-SNAPSHOT'
+    version '1.0.1'
     url 'https://github.com/famiprog/redmine-last-seen-issues'
     author_url 'https://github.com/famiprog'
   
